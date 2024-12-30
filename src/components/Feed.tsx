5f66e883fcb4b3113ad72dc481ab4e12c8023840
BEaANIKET <aniketchaturvedi309@gmail.com>
2024-12-30 20:57:58 +0530

    resolev margin

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -8,7 +8,7 @@ const Post = lazy(() => import('./Post'));
 export default function Feed() {
   return (
     <div className="max-w-4xl mx-auto pt-4 px-4 md:pt-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
-      <div className="lg:col-span-2 mt-16 md:mt-0">
+      <div className="lg:col-span-2 ml-auto mr-auto mt-16 md:mt-0 max-w-lg ">
         <Suspense fallback={<div className="w-full h-full flex justify-center items-center text-black">Loading...</div>}>
           {posts?.length ? (
             posts.map((post) => <Post key={post.id} post={post} />)
@@ -17,7 +17,7 @@ export default function Feed() {
           )}
         </Suspense>
       </div>
-      <div className="hidden lg:block">
+      <div className=" hidden lg:block">
         <Suggestions />
       </div>
     </div>
