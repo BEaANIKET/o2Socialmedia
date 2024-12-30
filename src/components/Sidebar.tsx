9e7a00f30e90393962d160e40ddb4e534826772d
BEaANIKET <aniketchaturvedi309@gmail.com>
2024-12-30 12:58:31 +0530

    change logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,37 +1,49 @@
 import { Link, useLocation } from 'react-router-dom';
 import { Home, Search, Compass, Film, Heart, PlusSquare, MessageCircle, Menu } from 'lucide-react';
 import ThemeToggle from './layout/ThemeToggle';
+import applogo from '../../public/assets/applogo.png'
 
 export default function Sidebar() {
   const location = useLocation();
 
   return (
-    <div className="fixed bottom-0 z-20 left-0 w-full bg-white dark:bg-gray-800 border-t dark:border-gray-700 md:top-0 md:w-16 lg:w-64 md:h-screen md:border-r ">
-      <div className="flex justify-around md:flex-col  h-full md:justify-start md:items-center lg:items-start md:p-4 md:space-y-6">
-        <div className="hidden lg:block p-4">
-          <h1 className="text-xl font-bold dark:text-white">Indimart</h1>
-        </div>
+    <>
 
-        <NavItem to="/" icon={<Home />} label="Home" active={location.pathname === '/'} />
-        <NavItem to="/search" icon={<Search />} label="Search" />
-        <NavItem to="/explore" icon={<Compass />} label="Explore" />
-        <NavItem to="/reels" icon={<Film />} label="Reels" active={location.pathname === '/reels'} />
-        {/* <NavItem to="/messages" icon={<MessageCircle />} label="Messages" /> */}
-        <NavItem to="/notifications" icon={<Heart />} label="Notifications" />
-        {/* <NavItem to="/create" icon={<PlusSquare />} label="Create" /> */}
+      <img className=' w-24 scale-110 absolute z-50 top-6 left-4 md:hidden ' src={applogo} alt="" />
 
-        <div className=' md:w-full w-fit flex lg:flex-row md:flex-col gap-4 justify-between items-center '>
-          <div className=' flex items-center justify-center'>
-            <NavItem to="/more" icon={<Menu />} label="More" />
+      <div className="fixed bottom-0 z-20 left-0 w-full bg-white dark:bg-gray-800 border-t dark:border-gray-700 md:top-0 md:w-16 lg:w-64 md:h-screen md:border-r ">
+
+
+        <div className="flex justify-around md:flex-col  h-full md:justify-start md:items-center lg:items-start md:p-4 md:space-y-6">
+          <div className="hidden lg:block p-4">
+            <img className=' w-20 scale-110 ' src={applogo} alt="" />
+            {/* <h1 className="text-xl font-bold dark:text-white">ReelDekho</h1> */}
           </div>
 
-          <div className="hidden md:flex md:items-center h-full  ">
-            <ThemeToggle />
-          </div>
-        </div>
 
+
+          <NavItem to="/" icon={<Home />} label="Home" active={location.pathname === '/'} />
+          <NavItem to="/search" icon={<Search />} label="Search" />
+          <NavItem to="/explore" icon={<Compass />} label="Explore" />
+          <NavItem to="/reels" icon={<Film />} label="Reels" active={location.pathname === '/reels'} />
+          {/* <NavItem to="/messages" icon={<MessageCircle />} label="Messages" /> */}
+          <NavItem to="/notifications" icon={<Heart />} label="Notifications" />
+          {/* <NavItem to="/create" icon={<PlusSquare />} label="Create" /> */}
+
+          <div className=' md:w-full w-fit flex lg:flex-row md:flex-col gap-4 justify-between items-center '>
+            <div className=' flex items-center justify-center'>
+              <NavItem to="/more" icon={<Menu />} label="More" />
+            </div>
+
+            <div className="hidden md:flex md:items-center h-full  ">
+              <ThemeToggle />
+            </div>
+          </div>
+
+        </div>
       </div>
-    </div>
+    </>
+
   );
 }
 
