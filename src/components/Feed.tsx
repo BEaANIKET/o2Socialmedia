329049bbd9f543d5216d965ffbe5147e012061ce
BEaANIKET <aniketchaturvedi309@gmail.com>
2024-12-30 13:03:07 +0530

    remove margi

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -6,7 +6,7 @@ import { posts } from '../data/dummyData';
 export default function Feed() {
   return (
     <div className="max-w-4xl mx-auto pt-4 px-4 md:pt-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
-      <div className="lg:col-span-2 mt-16">
+      <div className="lg:col-span-2 mt-16 md:mt-0">
         {/* <Stories /> */}
         {posts.map((post) => (
           <Post key={post.id} post={post} />
